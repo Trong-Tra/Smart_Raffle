2b1edbce621fa20ecc03d9d2645b838891eae29a
Minh Trong <traminhtrong2604@gmail.com>
2024-01-29 14:36:58 +0700

    commit #6

--- contracts/Raffle.sol
+++ contracts/Raffle.sol
@@ -8,11 +8,11 @@
 
 pragma solidity ^0.8.7;
 
-import "@chainlink/contract/src/v0.8/VRFConsumerBaseV2.sol"
+import "@chainlink/contracts/src/v0.8/vrf/VRFConsumerBaseV2.sol";
 
 error Raffle__NotEnoughETHEntered();
 
-contract Raffle is VRFConsumerBaseV2{
+contract Raffle is VRFConsumerBaseV2 {
     /* State Variables*/
     uint256 private immutable i_entranceFee;
     address payable[] private s_players;
@@ -21,31 +21,30 @@ contract Raffle is VRFConsumerBaseV2{
     event RaffleEnter(address indexed player);
 
     constructor(address vrfCoordinatorV2, uint256 entranceFee) VRFConsumerBaseV2(vrfCoordinatorV2) {
-        i_entranceFee = enterRaffle;
+        i_entranceFee = entranceFee;
     }
 
-    funciton enterRaffle(){
-        if(msg.value < i_entranceFee){
+    function enterRaffle() public payable {
+        if (msg.value < i_entranceFee) {
             revert Raffle__NotEnoughETHEntered();
         }
         s_players.push(payable(msg.sender));
         emit RaffleEnter(msg.sender);
     }
 
-    function requestRandomWinner() external {
+    function requestRandomWinner() external {}
 
-    }
-
-    function fulfillRandomWords(uint256 requestId, uint256[] memory randomWords) internal override{
-
-    }
+    function fulfillRandomWords(
+        uint256 requestId,
+        uint256[] memory randomWords
+    ) internal override {}
 
     /* View/Pure functions */
     function getEntranceFee() public view returns (uint256) {
         return i_entranceFee;
     }
 
-    function getPlayer(uint256 index) public view returns (address){
+    function getPlayer(uint256 index) public view returns (address) {
         return s_players[index];
     }
 }
