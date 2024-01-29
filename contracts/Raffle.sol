0b345dcc4d80563039d9a4507a891c7e3d3aecd5
Minh Trong <traminhtrong2604@gmail.com>
2024-01-29 13:42:43 +0700

    commit #5

---
+++ contracts/Raffle.sol
@@ -0,0 +1,51 @@
+// Raffle
+// Enter the lottery (paying some amount)
+// pick a random winner (verifiably random)
+// winner to be selected every X minutes -> completly automate
+// Chainlink Oracle -> Randomness, Automated Execution (Chainlink Keeper)
+
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.7;
+
+import "@chainlink/contract/src/v0.8/VRFConsumerBaseV2.sol"
+
+error Raffle__NotEnoughETHEntered();
+
+contract Raffle is VRFConsumerBaseV2{
+    /* State Variables*/
+    uint256 private immutable i_entranceFee;
+    address payable[] private s_players;
+
+    /* Events */
+    event RaffleEnter(address indexed player);
+
+    constructor(address vrfCoordinatorV2, uint256 entranceFee) VRFConsumerBaseV2(vrfCoordinatorV2) {
+        i_entranceFee = enterRaffle;
+    }
+
+    funciton enterRaffle(){
+        if(msg.value < i_entranceFee){
+            revert Raffle__NotEnoughETHEntered();
+        }
+        s_players.push(payable(msg.sender));
+        emit RaffleEnter(msg.sender);
+    }
+
+    function requestRandomWinner() external {
+
+    }
+
+    function fulfillRandomWords(uint256 requestId, uint256[] memory randomWords) internal override{
+
+    }
+
+    /* View/Pure functions */
+    function getEntranceFee() public view returns (uint256) {
+        return i_entranceFee;
+    }
+
+    function getPlayer(uint256 index) public view returns (address){
+        return s_players[index];
+    }
+}
---
+++ hardhat.config.js
@@ -0,0 +1,11 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+require("hardhat-gas-reporter");
+require("hardhat-deploy");
+require("solidity-coverage");
+require("hardhat-contract-sizer");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.19",
+};
