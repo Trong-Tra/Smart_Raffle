4148ce9b82bb3e6cf21634528d311623a764e44b
Minh Trong <traminhtrong2604@gmail.com>
2024-01-30 18:14:23 +0700

    build: checkUpkeep function

--- contracts/Raffle.sol
+++ contracts/Raffle.sol
@@ -36,6 +36,8 @@ contract Raffle is VRFConsumerBaseV2, AutomationCompatible {
     /* Lottery Variables */
     address private s_recentWinner;
     RaffleState private s_raffleState;
+    uint256 private s_lastTimeStamp;
+    uint256 private s_interval;
 
     /* Events */
     event RaffleEnter(address indexed player);
@@ -47,7 +49,8 @@ contract Raffle is VRFConsumerBaseV2, AutomationCompatible {
         uint256 entranceFee,
         bytes32 gasLane,
         uint64 subscriptionId,
-        uint32 callbackGasLimit
+        uint32 callbackGasLimit,
+        uint256 interval
     ) VRFConsumerBaseV2(vrfCoordinatorV2) {
         i_entranceFee = entranceFee;
         i_vrfCoordinatior = VRFCoordinatorV2Interface(vrfCoordinatorV2);
@@ -55,6 +58,8 @@ contract Raffle is VRFConsumerBaseV2, AutomationCompatible {
         i_subscriptionId = subscriptionId;
         i_callbackGasLimit = callbackGasLimit;
         s_raffleState = RaffleState.OPEN;
+        s_lastTimeStamp = block.timestamp;
+        s_interval = interval;
     }
 
     function enterRaffle() public payable {
@@ -78,7 +83,15 @@ contract Raffle is VRFConsumerBaseV2, AutomationCompatible {
      * 4. The lottery should be in an "open" state
      */
 
-    function checkUpkeep(bytes calldata /* checkData */) external override {}
+    function checkUpkeep(
+        bytes calldata /* check Data */
+    ) external override returns (bool upkeepNeeded, bytes memory /* perform Data */) {
+        bool isOpen = (RaffleState.OPEN == s_raffleState);
+        bool timePassed = ((block.timestamp - s_lastTimeStamp) > s_interval);
+        bool hasPlayers = (s_players.length > 0);
+        bool hasBalance = address(this).balance > 0;
+        upkeepNeeded = (isOpen && timePassed && hasPlayers && hasBalance);
+    }
 
     function requestRandomWinner() external {
         s_raffleState = RaffleState.CALCULATING;
