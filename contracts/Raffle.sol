8f61af43ecb9580d0062b4bc5849e812c28a4545
Minh Trong <traminhtrong2604@gmail.com>
2024-01-31 12:41:52 +0700

    build: new error in case performUpkeep call checkUpkeep and recieved false upkeepNeeded

--- contracts/Raffle.sol
+++ contracts/Raffle.sol
@@ -15,6 +15,7 @@ import "@chainlink/contracts/src/v0.8/automation/interfaces/AutomationCompatible
 error Raffle__NotEnoughETHEntered();
 error Raffle__TransferFailed();
 error Raffle__NotOpen();
+error Raffle__UpkeepNotNeeded(uint256 currentBalance, uint256 numPlayers, uint256 raffleState);
 
 contract Raffle is VRFConsumerBaseV2, AutomationCompatibleInterface {
     /* Type declarations */
@@ -95,6 +96,13 @@ contract Raffle is VRFConsumerBaseV2, AutomationCompatibleInterface {
 
     function performUpkeep(bytes calldata /* performData */) external override {
         (bool upkeepNeeded, ) = checkUpkeep("");
+        if (!upkeepNeeded) {
+            revert Raffle__UpkeepNotNeeded(
+                address(this).balance,
+                s_players.length,
+                uint256(s_raffleState)
+            );
+        }
         s_raffleState = RaffleState.CALCULATING;
         uint256 requestId = i_vrfCoordinatior.requestRandomWords(
             i_gasLane,
@@ -115,6 +123,7 @@ contract Raffle is VRFConsumerBaseV2, AutomationCompatibleInterface {
         s_recentWinner = recentWinner;
         s_raffleState = RaffleState.OPEN;
         s_players = new address payable[](0);
+        s_lastTimeStamp = block.timestamp;
         (bool success, ) = recentWinner.call{value: address(this).balance}("");
         if (!success) {
             revert Raffle__TransferFailed();
