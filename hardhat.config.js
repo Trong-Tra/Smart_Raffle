d445e26fe62e524572a16bc1f70c42a3600a3007
Minh Trong <traminhtrong2604@gmail.com>
2024-01-31 13:15:13 +0700

    build: deploy-01, raffle deployment

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,7 +5,32 @@ require("hardhat-deploy")
 require("solidity-coverage")
 require("hardhat-contract-sizer")
 
-/** @type import('hardhat/config').HardhatUserConfig */
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL
+const PRIVATE_KEY = process.env.PRIVATE_KEY
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
+
 module.exports = {
-    solidity: "0.8.19",
+    solidity: "0.8.7",
+    defaultNetwork: "hardhat",
+    networks: {
+        hardhat: {
+            chainId: 31337,
+            blockConfirmations: 1,
+        },
+        sepolia: {
+            url: SEPOLIA_RPC_URL,
+            accounts: [PRIVATE_KEY],
+            chainId: 11155111,
+            blockConfirmations: 6,
+        },
+    },
+    namedAccounts: {
+        deployer: {
+            default: 0,
+        },
+        player: {
+            default: 1,
+        },
+    },
 }
