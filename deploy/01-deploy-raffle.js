7d2c4e1e688c1f82f2083aa017fee6b7b4205470
Minh Trong <traminhtrong2604@gmail.com>
2024-02-01 10:37:11 +0700

    build: argument subscriptionId

--- deploy/01-deploy-raffle.js
+++ deploy/01-deploy-raffle.js
@@ -1,22 +1,29 @@
 const { network, ethers } = require("hardhat")
 const { developmentChains, networkConfig } = require("../helper-hardhat-config")
 
+const VRF_SUB_FUND_AMOUNT = ethers.parseEther("2")
 module.exports = async ({ getNamedAccounts, deployments }) => {
     const { deploy, log } = deployments
     const { deployer } = await getNamedAccounts()
     const chainId = network.config.chainId
-    let vrfCoordinatorV2Address
+    let vrfCoordinatorV2Address, subscriptionId
 
     if (developmentChains.includes(network.name)) {
         const vrfCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock")
         vrfCoordinatorV2Address = vrfCoordinatorV2Mock.address
+        const TxResponse = await vrfCoordinatorV2Mock.createSubscription()
+        const TxReceipt = await TxResponse.wait(1)
+        subscriptionId = TxReceipt.events[0].args.subId
+        await vrfCoordinatorV2Mock.fundSubscription(subscriptionId, VRF_SUB_FUND_AMOUNT)
     } else {
         vrfCoordinatorV2Address = networkConfig[chainId]["vrfCoordinatorV2"]
+        subscriptionId = networkConfig[chainId]["subscriptionId"]
     }
 
     const entranceFee = networkConfig[chainId]["entranceFee"]
     const gasLane = networkConfig[chainId]["gasLane"]
-    const args = [vrfCoordinatorV2Address, entranceFee, gasLane]
+
+    const args = [vrfCoordinatorV2Address, entranceFee, gasLane, subscriptionId]
     const raffle = await deploy("Raffle", {
         from: deployer,
         args: args,
--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -6,6 +6,7 @@ const networkConfig = {
         vrfCoordinatorV2: "0x8103B0A8A00be2DDC778e6e7eaa21791Cd364625",
         entranceFee: ethers.parseEther("0.01"),
         gasLane: "0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c", // 150 gwei key hash
+        subscriptionId: "1",
     },
     31337: {
         name: "hardhat",
