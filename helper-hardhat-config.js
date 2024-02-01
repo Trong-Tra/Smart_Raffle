370ba90f3d03e0263aefe23b0291871b46443479
Minh Trong <traminhtrong2604@gmail.com>
2024-02-01 10:05:20 +0700

    build: contract deployment and add default network for config file

--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -1,7 +1,14 @@
+const { ethers } = require("hardhat")
+
 const networkConfig = {
     11155111: {
         name: "sepolia",
         vrfCoordinatorV2: "0x8103B0A8A00be2DDC778e6e7eaa21791Cd364625",
+        entranceFee: ethers.parseEther("0.01"),
+    },
+    31337: {
+        name: "hardhat",
+        entranceFee: ethers.parseEther("0.01"),
     },
 }
 
